cacad97ac3bc8011cef2e5b3e897fbdfaccf8ccb
falah99-cik <ahmadmathlaulfalah14@gmail.com>
2025-12-03 20:24:41 +0700

    ternyata pada foldeer kategori

--- resources/views/admin/jenis_hewan/create.blade.php
+++ resources/views/admin/jenis_hewan/create.blade.php
@@ -1,52 +1,41 @@
 @extends('layouts.lte.main')
 
 @section('content-header')
+    <div class="row">
+        <div class="col-sm-6">
+            <h3 class="mb-0">Tambah Jenis Hewan</h3>
+        </div>
 
-<div class="container-fluid px-4 pt-2 mt-3">
-
-<div class="row">
-    <div class="col-sm-6">
-        <h3 class="mb-0">Tambah Jenis Hewan</h3>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-end">
+                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.jenis_hewan.index') }}">Jenis Hewan</a></li>
+                <li class="breadcrumb-item active">Tambah</li>
+            </ol>
+        </div>
     </div>
-    <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-end">
-            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('admin.jenis_hewan.index') }}">Jenis Hewan</a></li>
-            <li class="breadcrumb-item active">Tambah</li>
-        </ol>
-    </div>
-</div>
 @endsection
 
 @section('content')
 
 <div class="card">
     <div class="card-header">
-        <h5 class="mb-0">Form Tambah Jenis Hewan</h5>
+        <h4 class="card-title mb-0">Form Tambah Jenis Hewan</h4>
     </div>
 
     <div class="card-body">
-
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <strong>Periksa kembali input Anda:</strong>
-                <ul class="mb-0 mt-2">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
 
         <form action="{{ route('admin.jenis_hewan.store') }}" method="POST">
             @csrf
 
             <div class="mb-3">
                 <label class="form-label">Nama Jenis Hewan</label>
-                <input type="text" name="nama_jenis_hewan"
-                       class="form-control @error('nama_jenis_hewan') is-invalid @enderror"
-                       placeholder="Contoh: Anjing, Kucing, Sapi"
-                       value="{{ old('nama_jenis_hewan') }}">
+                <input 
+                    type="text" 
+                    name="nama_jenis_hewan" 
+                    class="form-control @error('nama_jenis_hewan') is-invalid @enderror"
+                    value="{{ old('nama_jenis_hewan') }}"
+                    placeholder="Contoh: Kucing, Anjing, Kelinci">
 
                 @error('nama_jenis_hewan')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -59,12 +48,11 @@
                 </a>
 
                 <button type="submit" class="btn btn-primary">
-                    <i class="bi bi-save"></i> Simpan
+                    <i class="bi bi-check2-circle"></i> Simpan
                 </button>
             </div>
 
         </form>
-
     </div>
 </div>
 
